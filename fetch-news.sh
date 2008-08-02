ba503278cf1e3518c11f70dedd2c6bd98954f8ed
tailor <trebbu@googlemail.com>
2008-08-02 21:58:35 +0200

    toc partly abbreviated, lots of typos fixed

--- fetch-news.sh
+++ fetch-news.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 #
-#       Fetch news
+#       Fetch news and prune them for inclusion by html/index.php
 #
-/usr/bin/wget -q -O news-clqr.tmp 'http://developer.berlios.de/export/projnews.php?group_id=9765&limit=10&flat=1&show_summaries=1'  > /dev/null
+/usr/bin/wget -q -O news-clqr.tmp \
+    'http://developer.berlios.de/export/projnews.php?group_id=9765&limit=10&flat=1&show_summaries=1' \
+    > /dev/null
 /bin/sed -e "s/  &nbsp; - &nbsp; <a href=\"\/\/developer.berlios.de\/projects\/clqr\/\">Common Lisp Quick Reference<\/a>//g"\
     -e "s/<a [^>]*>//g" -e "s/<\/a>//g"\
     -e "s/<b>/<h5>/g" -e "s/<\/b>/<\/h5>/g" \
@@ -10,3 +12,4 @@
     -e "s/<div .*<\/div>//g" -e "s/<hr [^>]*>/<p>/g"\
     -e "s/^[ \t\]*//g" -e "s/&nbsp;&nbsp;&nbsp;//g"\
     news-clqr.tmp > /home/groups/clqr/htdocs/news-clqr.html
+/bin/rm news-clqr.tmp
